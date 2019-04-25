3c63f0ba7d225c732e6eba2002f2fd0fa2c8fd7c
Tanmaya Nanda <tanmaya@Tanmayas-MacBook-Pro.local>
2019-04-25 13:51:17 -0400

    Add: entity bean for file mapping

--- src/com/tanmaya/projects/cloudnine/bean/FileMapping.java
+++ src/com/tanmaya/projects/cloudnine/bean/FileMapping.java
@@ -1,5 +1,45 @@
 package com.tanmaya.projects.cloudnine.bean;
 
 public class FileMapping {
+	private int id;
+	private String filepath;
+	private String filename;
+
+	public FileMapping(int id, String filepath, String filename) {
+		super();
+		this.id = id;
+		this.filepath = filepath;
+		this.filename = filename;
+	}
+
+	public FileMapping(String filepath, String filename) {
+		super();
+		this.filepath = filepath;
+		this.filename = filename;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
 
 }
