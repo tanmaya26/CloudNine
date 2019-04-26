1f83ed027e7b6257eb8d34b86d55743a41aee28a
Maharsh <maharsh312@gmail.com>
2019-04-25 21:51:20 -0400

    Added DirectoryList Function

---
+++ src/com/tanmaya/projects/cloudnine/dao/DirectoryDAO.java
@@ -0,0 +1,64 @@
+package com.tanmaya.projects.cloudnine.dao;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tanmaya.projects.cloudnine.bean.FileMeta;
+
+public class DirectoryDAO {
+	public static final String jdbcURL = "jdbc:mysql://localhost:3306/photobucket";
+	public static final String user = "root";
+	public static final String password = "";
+	Connection connection = null;
+	Statement statement = null;
+	ResultSet result = null;
+
+	public List<String> listDirectory() {
+		List <String> dirList = new ArrayList<>();
+		try {
+			File currDir = new File(".");
+			File[] filesList = currDir.listFiles();
+			for(File f : filesList) {
+				if(f.isDirectory()) {
+					dirList.add(f.getPath());
+				}
+			}
+			
+		} catch (Throwable oops) {
+			oops.printStackTrace();
+		}
+		return dirList;
+	}
+
+	static void close(Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	static void close(Statement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	static void close(ResultSet result) {
+		if (result != null) {
+			try {
+				result.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+}
