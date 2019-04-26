2141a6fe0fee24c1d209e27c98bace1a28602de2
Tanmaya Nanda <tanmaya@tanmayas-mbp.home>
2019-04-25 21:54:38 -0400

    Add: isDeleted attribute for FileMapping

--- src/com/tanmaya/projects/cloudnine/bean/FileMapping.java
+++ src/com/tanmaya/projects/cloudnine/bean/FileMapping.java
@@ -4,18 +4,21 @@ public class FileMapping {
 	private int id;
 	private String filepath;
 	private String filename;
+	private int isDeleted;
 
-	public FileMapping(int id, String filepath, String filename) {
+	public FileMapping(int id, String filepath, String filename, int isDeleted) {
 		super();
 		this.id = id;
 		this.filepath = filepath;
 		this.filename = filename;
+		this.isDeleted = isDeleted;
 	}
 
-	public FileMapping(String filepath, String filename) {
+	public FileMapping(String filepath, String filename, int isDeleted) {
 		super();
 		this.filepath = filepath;
 		this.filename = filename;
+		this.isDeleted = isDeleted;
 	}
 
 	public int getId() {
@@ -41,5 +44,14 @@ public class FileMapping {
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
+
+	public int getIsDeleted() {
+		return isDeleted;
+	}
+
+	public void setIsDeleted(int isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	
 
 }
