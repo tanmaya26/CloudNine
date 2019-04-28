34e81d505022a990f12d57beaff8ff6db4b03ca4
Maharsh <maharsh312@gmail.com>
2019-04-27 21:42:18 -0400

    dedulication completed

--- src/com/tanmaya/projects/cloudnine/dao/FileListDAO.java
+++ src/com/tanmaya/projects/cloudnine/dao/FileListDAO.java
@@ -10,22 +10,24 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
 
 import com.tanmaya.projects.cloudnine.bean.FileList;
 import com.tanmaya.projects.cloudnine.bean.FileMeta;
 
-
 public class FileListDAO {
 	Config conf = null;
-	public  String jdbcURL;
-	public  String user;
-	public  String password;
-	public FileListDAO(){
+	public String jdbcURL;
+	public String user;
+	public String password;
+
+	public FileListDAO() {
 		conf = new Config();
 		jdbcURL = conf.getJdbcURL();
 		user = conf.getUser();
 		password = conf.getPassword();
 	}
+
 	Connection connection = null;
 	Statement statement = null;
 	ResultSet result = null;
@@ -38,8 +40,9 @@ public class FileListDAO {
 			try {
 				connection = DriverManager.getConnection(jdbcURL, user, password);
 				statement = connection.createStatement();
-				ResultSet rs = statement.executeQuery("select file_mappings.id, filename, file_metadata.id, date_modified, size, extension, owner from file_mappings, file_metadata where file_mappings.id = file_metadata.mapping_id AND is_deleted = 0;");
-				while(rs.next()) {
+				ResultSet rs = statement.executeQuery(
+						"select file_mappings.id, filename, file_metadata.id, date_modified, size, extension, owner from file_mappings, file_metadata where file_mappings.id = file_metadata.mapping_id AND is_deleted = 0;");
+				while (rs.next()) {
 					int fid = rs.getInt(1);
 					String fname = rs.getString("filename");
 					int fmid = rs.getInt(3);
@@ -59,34 +62,44 @@ public class FileListDAO {
 		}
 		return fileList;
 	}
-	
+
 	public void deDuplicate(String absoluteBasePath) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			try {
-				HashSet<String> hashList = new HashSet<String>();
+				HashMap<String,String> hashList = new HashMap<String,String>();
 				connection = DriverManager.getConnection(jdbcURL, user, password);
 				statement = connection.createStatement();
-				ResultSet rs = statement.executeQuery("select file_metadata.id, hash, filepath from file_mappings, file_metadata where file_mappings.id = file_metadata.mapping_id;");
-				while(rs.next()) {
+				ResultSet rs = statement.executeQuery(
+						"select file_metadata.id, hash, is_dedup, filepath from file_mappings, file_metadata where file_mappings.id = file_metadata.mapping_id AND is_dedup = 0;");
+				HashMap<String, Integer> deDup = new HashMap<String, Integer>();
+				while (rs.next()) {
 					int fid = rs.getInt(1);
 					String hash = rs.getString("hash");
-					if(hashList.contains(hash)) {
-						System.out.println(fid+" has to be deleted");
-						String relativeFilePath = rs.getString("filepath");
+					int isDedup = rs.getInt(3);
+					String relativeFilePath = rs.getString("filepath");
+					String filePath = relativeFilePath;
+					if (hashList.containsKey(hash) && isDedup == 0) {
+						System.out.println(fid + " has to be deleted");
 						relativeFilePath = relativeFilePath.replace('/', '\\');
 						String adjustedFilePath = absoluteBasePath + relativeFilePath.substring(1);
 						System.out.println("File to be deleted from: " + adjustedFilePath);
 						File file = new File(adjustedFilePath);
-						if(file.delete());
-						{
+						if (file.delete()) {
 							System.out.println("File Deleted");
+							deDup.put(hashList.get(hash), fid);
+						} else {
+							System.out.println("File Not Found");
 						}
-					}
-					else {
-						hashList.add(hash);
+					} else{
+						hashList.put(hash,filePath);
 					}
 				}
+				for (Entry<String, Integer> entry : deDup.entrySet()) {
+					statement.executeUpdate("UPDATE file_mappings SET is_dedup = 1 , link = '" + entry.getKey()
+							+ "' where id = " + entry.getValue() + ";");
+				}
+
 			} finally {
 				close(result);
 				close(statement);
